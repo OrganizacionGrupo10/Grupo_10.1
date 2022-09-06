2e34cad45d605cb10ecbd752a11c5262befbfe41
wleonard0 <89997031+wleonard0@users.noreply.github.com>
2022-09-05 21:29:56 -0500

    Initial commit

---
+++ src/main/java/com/grupo_10/grupo_10/Grupo10Application.java
@@ -0,0 +1,13 @@
+package com.grupo_10.grupo_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo_10/grupo_10/Grupo10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo_10.grupo_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
