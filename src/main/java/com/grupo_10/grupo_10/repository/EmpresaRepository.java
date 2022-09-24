aeff723a5a5d54360810b8512f4af5745cf15632
Jorge Cuervo <111306840+jecuervoa@users.noreply.github.com>
2022-09-24 11:48:22 -0500

    Delete src directory

--- src/main/java/com/grupo_10/grupo_10/repository/EmpresaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.grupo_10.grupo_10.repository;
-
-import com.grupo_10.grupo_10.model.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {}
--- src/test/java/com/grupo_10/grupo_10/Grupo10ApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.grupo_10.grupo_10;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Grupo10ApplicationTests {
-
-}
